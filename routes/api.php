b5269342a2e8fc8bdc5b849e17142b48ba2c8e72
brahim lmlilas <spedibra@gmail.com>
2025-03-11 16:06:07 +0100

    GM-2

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\Admin\AuthController;
+use App\Http\Controllers\Api\V1\Admin\DashboardController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -10,4 +12,19 @@ Route::get('/user', function (Request $request) {
     return ['message' => 'You are authenticated'];
 });
 
+Route::get('/test',function (Request $request) {
+    return response()->json(['message' => 'oui ']);
+});
 
+Route::prefix('v1')->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', [AuthController::class, 'logout']);
+            
+            Route::get('/dashboard', [DashboardController::class, 'index']);
+        });
+    });
+});
