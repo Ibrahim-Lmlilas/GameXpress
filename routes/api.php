2fadf5098d08d7d60d4e4a83fc7286dce21920b1
brahim lmlilas <spedibra@gmail.com>
2025-03-16 11:00:26 +0100

    role_and_permission

--- routes/api.php
+++ routes/api.php
@@ -26,10 +26,42 @@ Route::prefix('v1')->group(function () {
 
         Route::middleware('auth:sanctum')->group(function () {
             Route::post('/logout', [AuthController::class, 'logout']);
-            Route::get('/dashboard', [DashboardController::class, 'index']);
-            Route::apiResource('products', ProductController::class);
-            Route::apiResource('categories', CategoryController::class);
-            Route::apiResource('users', UserController::class);
+
+            Route::get('/dashboard', [DashboardController::class, 'index'])
+            ->middleware(['role:super_admin', 'permission:view_dashboard']);
+
+            Route::get('products', [ProductController::class, 'index'])
+            ->middleware(['role:super_admin', 'permission:view_products']);
+            Route::post('products', [ProductController::class, 'store'])
+            ->middleware(['role:super_admin', 'permission:create_products']);
+            Route::get('products/{id}', [ProductController::class, 'show'])
+            ->middleware(['role:super_admin', 'permission:view_products']);
+            Route::put('products/{id}', [ProductController::class, 'update'])
+            ->middleware(['role:super_admin', 'permission:edit_products']);
+            Route::delete('products/{id}', [ProductController::class, 'destroy'])
+            ->middleware(['role:super_admin','permission:delete_products']);
+
+            Route::get('categories', [CategoryController::class, 'index'])
+            ->middleware(['role:super_admin','permission:view_categories']);
+            Route::post('categories', [CategoryController::class, 'store'])
+            ->middleware(['role:super_admin','permission:create_categories']);
+            Route::get('categories/{id}', [CategoryController::class, 'show'])
+            ->middleware(['role:super_admin','permission:view_categories']);
+             Route::put('categories/{id}', [CategoryController::class, 'update'])
+            ->middleware(['role:super_admin','permission:edit_categories']);
+            Route::delete('categories/{id}', [CategoryController::class, 'destroy'])
+            ->middleware(['role:super_admin','permission:delete_categories']);
+
+            Route::get('users', [UserController::class, 'index'])
+            ->middleware(['role:super_admin','permission:view_users']);
+            Route::post('users', [UserController::class, 'store'])
+            ->middleware(['role:super_admin','permission:create_users']);
+            Route::get('users/{id}', [UserController::class, 'show'])
+            ->middleware(['role:super_admin','permission:view_users']);
+            Route::put('users/{id}', [UserController::class, 'update'])
+            ->middleware(['role:super_admin','permission:edit_users']);
+            Route::delete('users/{id}', [UserController::class, 'destroy'])
+            ->middleware(['role:super_admin','permission:delete_users']);
 
         });
     });
